42d6a85457b4474c6a06e434aa3112dbadf63579
xhanyifeng <xhanyifeng@gmail.com>
2020-07-05 23:47:40 +0800

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,14 @@
 set -eu
 
 # Variables.
-GITHUB_WORKSPACE="."
-PIC_PATH="$GITHUB_WORKSPACE/$INPUT_PIC_PATH"
-COMP_LEVEL="$INPUT_COMPRESS_LEVEL" 
+#PIC_PATH="$GITHUB_WORKSPACE/$INPUT_PIC_PATH"
+#COMP_LEVEL="$INPUT_COMPRESS_LEVEL" 
 
 # Compress pic
-for file in `find $INPUT_PIC_PATH -regex '.*\.png\|.*\.jpg'`; do 
+for file in `find $GITHUB_WORKSPACE/$INPUT_PIC_PATH -regex '.*\.png\|.*\.jpg'`; do 
     fileType=`file -i $file  | awk '{print substr($2, 1, length($2)-1)}'| cut -d'/' -f2`;
     if [[ $fileType == "png" ]]; then
-        optipng -o $COMP_LEVEL $file;
+        optipng -o $INPUT_COMP_LEVEL $file;
     elif [[ $fileType == "jpeg" ]]; then
         jpegoptim $file; 
     fi;
