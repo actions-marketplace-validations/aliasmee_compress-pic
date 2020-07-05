10d65ee46817cfc762aa8639ca48305cddba2d11
xhanyifeng <xhanyifeng@gmail.com>
2020-07-05 23:33:48 +0800

    try fix env

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@
 set -eu
 
 # Variables.
+GITHUB_WORKSPACE="."
 PIC_PATH="$GITHUB_WORKSPACE/$INPUT_PIC_PATH"
 COMP_LEVEL="$INPUT_COMPRESS_LEVEL" 
 
 # Compress pic
-for file in `find $PIC_PATH -regex '.*\.png\|.*\.jpg'`; do 
+for file in `find $INPUT_PIC_PATH -regex '.*\.png\|.*\.jpg'`; do 
     fileType=`file -i $file  | awk '{print substr($2, 1, length($2)-1)}'| cut -d'/' -f2`;
     if [[ $fileType == "png" ]]; then
         optipng -o $COMP_LEVEL $file;
